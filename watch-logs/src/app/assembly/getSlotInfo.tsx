49368586302d4a78a5ec76dcf6ea046e692865a1
adair_chan <adair_chan@hotmail.com>
2024-07-25 10:22:32 +0800

    feat: assembly test

--- watch-logs/src/app/assembly/getSlotInfo.tsx
+++ watch-logs/src/app/assembly/getSlotInfo.tsx
@@ -3,8 +3,7 @@
  * @Date: 2024-07-24 18:07:52
  * @Description: 
  */
-// pages/api/getLockInfo.js
-import { createPublicClient, http, toHex, keccak256 } from 'viem'
+import { createPublicClient, http, hexToBigInt, toHex, padHex, keccak256 } from 'viem'
 import { sepolia } from 'viem/chains'
  
 const publicClient = createPublicClient({ 
@@ -13,8 +12,9 @@ const publicClient = createPublicClient({
 }) 
 
 const contractAddress = '0xB2B77A3c7600Aac203Bc1Bf7Cae12317efB3D508';
+const zeroFlag = padHex(toHex(0), {size: 32})
 // 计算数组的起始槽 
-const arrayStartSlot = keccak256('0x0000000000000000000000000000000000000000000000000000000000000000');
+const arrayStartSlot = keccak256(zeroFlag);
 
 // struct LockInfo{
 //     address user; 20 bytes
@@ -34,11 +34,6 @@ async function getLockInfo(index:any) {
     slot: toHex(userAndStartTimeSlot),
   });
 
-  if (userAndStartTimeSlotData === '0x0000000000000000000000000000000000000000000000000000000000000000') {
-    // 查不到数据就返回空
-    return null;
-  }
-
   const amountHex = await publicClient.getStorageAt({
     address: contractAddress,
     slot: toHex(amountSlot),
@@ -47,8 +42,10 @@ async function getLockInfo(index:any) {
   // 解析读取的数据 userAndStartTimeSlotData 类似: 0x0000000000000000000000000000000000000000000000000de0b6b3a7640000
   // 长度 66, 去除 0x 还有 64 位 
   const user = '0x' + userAndStartTimeSlotData!.slice(26); // 取最后 40 位字符（地址）
-  const startTime = new Date(parseInt(userAndStartTimeSlotData!.slice(10, 26), 16) * 1000).toLocaleString(); // 取前 16 位字符（时间戳）
-  const amount = BigInt(amountHex!);
+  let startTimeHex = userAndStartTimeSlotData!.slice(10, 26); // 取 16 位字符（时间戳）
+  const startTime = new Date(parseInt(startTimeHex, 16) * 1000).toLocaleString(); // 取前 16 位字符（时间戳）
+  const amount = hexToBigInt(amountHex as `0x${string}`);
+  console.log("====> amount",  hexToBigInt(amountHex as `0x${string}`))
   console.log("===========slotData", user, startTime, amount)
 
   return {
@@ -61,3 +58,11 @@ async function getLockInfo(index:any) {
 export const getSlotInfo = async (index: any) => {
     return getLockInfo(index);
 }
+
+export const getLength = async () => {
+    const data = await publicClient.getStorageAt({
+        address: contractAddress,
+        slot: '0x0'
+    })
+    return hexToBigInt(data as `0x${string}`)
+};
--- watch-logs/src/app/assembly/page.tsx
+++ watch-logs/src/app/assembly/page.tsx
@@ -1,7 +1,7 @@
 
 'use client'
 import { useEffect, useState } from 'react';
-import { getSlotInfo } from './getSlotInfo';
+import { getSlotInfo, getLength } from './getSlotInfo';
 import logStyle from '../../styles/logs.module.css';
 
 type LockInfo = {
@@ -13,12 +13,19 @@ type LockInfo = {
 export default function Home() {
   const [locks, setLocks] = useState<any>([]);
   const [index, setIndex] = useState(0);
+  const [length, setLength] = useState(0);
 
   useEffect(() => {
+   
     let isMounted = true; // 控制递归调用的局部变量
     async function fetchLocks(currentIndex: number) {
+      if (currentIndex == 0) {
+        // 获取 _locks 的长度
+        getLength().then( (len) => setLength(Number(BigInt(len))));
+        console.log("length===> ", length)
+      }
       const res: any = await getSlotInfo(currentIndex);
-      if (isMounted && res !== null) {
+      if (isMounted && currentIndex < length) {
         setLocks((prevLocks:any) => {
           return [...prevLocks, res];
         });
@@ -30,15 +37,16 @@ export default function Home() {
     return () => {
       isMounted = false; // 组件卸载时停止递归调用
     };
-  }, [index]);
+  }, [length, index]);
 
   return (
-    <div>
-      <h1>Lock Info</h1>
+    <div className={logStyle.container}>
+      <h1>Get Lock Info</h1>
       {locks.length === 0 ? (
         <p>Loading...</p>
       ) : (
         <div className={logStyle.container}>
+          <h1>共有Locks: <strong> {length} </strong></h1>
           <ul className={logStyle.resultList}>
             {locks && locks.map((lock: any, index: number) => (
               <li key={index} className={logStyle.resultItem2}>
